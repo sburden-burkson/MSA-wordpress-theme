127e2bc72bc6a0b26c9976d209bffc2b17925b65
Stephen Burden <spburden@hotmail.com>
2018-03-13 16:46:24 -0700

    add to section full acf

--- template-parts/section-parts/section-full.php
+++ template-parts/section-parts/section-full.php
@@ -1,31 +1,34 @@
 <?php
     //vars
-    $section_background = get_sub_field('section_background');
-    $section_type = $section_background ? "-bg " : "-section ";
-    $reduced_height = get_sub_field('reduced_height') ? "half" . $section_type : "full" . $section_type;
-    $shade_of_background = get_sub_field('shade_of_background') . " ";
-    $content_side = (get_sub_field('content_side') != "Center") ? get_sub_field('content_side') . " " : "";
-    $vertical_arrangement = get_sub_field('vertical_arrangement');
-    $content_width = get_sub_field('width_restricted_content') ? "restricted-width " : "";
-    $centered_text = get_sub_field('centered_text') ? "text-center " : "";
-    $spaced_out_text = get_sub_field('spaced_out_text') ? "spaced-section " : "";
     $section_content = get_sub_field('section_content');
+    $background_image = get_sub_field('background_image');
+    $shade_of_background = get_sub_field('shade_of_background') . " ";
+    $width_restriction = get_sub_field('width_restriction') . " ";
+    $centered_text = get_sub_field('entered_text') ? 'text-center ' : '';
+    $vertical_padding = get_sub_field('vertical_padding') . " ";
+    $texture_section = get_sub_field('texture_section') ? 'texture-section ' : '';
+    $overlay = get_sub_field('overlay');
+    $add_a_button = get_sub_field('add_a_button');
+    $button_text = get_sub_field('button_text');
+    $button_url = get_sub_field('button_url');
+    // $section_type = $section_background ? "-bg " : "-section ";
+    // $reduced_height = get_sub_field('reduced_height') ? "half" . $section_type : "full" . $section_type;
+
+    // $content_side = (get_sub_field('content_side') != "Center") ? get_sub_field('content_side') . " " : "";
+    // $vertical_arrangement = get_sub_field('vertical_arrangement');
+    // $content_width = get_sub_field('width_restricted_content') ? "restricted-width " : "";
+    // $texture_secttion = get_sub_field('centered_text') ? "text-center " : "";
+    // $spaced_out_text = get_sub_field('spaced_out_text') ? "spaced-section " : "";
+
 ?>
 
 <!-- Full Section -->
-<div class="<?php echo $shade_of_background . $reduced_height; ?>" style="background-image: url('<?php echo $section_background; ?>');">
-  <div class="container wp-full-height">
-    <div class="row wp-full-height">
-      <div class="<?php echo $content_side; ?>wp-full-height">
-        <div class="wp-table wp-full-height">
-          <div class="<?php echo $vertical_arrangement; ?>">
-            <div class="<?php echo $content_width . $spaced_out_text . $centered_text; ?>section-info-wrap">
-              <?php echo $section_content; ?>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+<div class="section <?php echo $shade_of_background . $texture_section; ?>" style="background-image: url('<?php echo $background_image; ?>');">
+  <div class="<?php echo $vertical_padding . $width_restriction . $centered_text . $overlay; ?>">
+    <?php echo $section_content; ?>
+    <?php if ($add_a_button): ?>
+      <p class="text-center pad-top"><a href="<?php echo $button_url; ?>" class="btn wp-btn-extra-long text-uppercase wp-btn-red"><?php echo $button_text; ?> <i class="fal fa-long-arrow-right fa-lg"></i></a></p>
+    <?php endif; ?>
   </div>
 </div>
 <!-- /Full Section -->
