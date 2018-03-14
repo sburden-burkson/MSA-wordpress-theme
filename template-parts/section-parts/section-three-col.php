adeb7ab26ef84637bbf52d4f2cb3c1e8977d7cdf
Stephen Burden <spburden@hotmail.com>
2018-03-14 12:34:59 -0700

    add 3 column section

--- template-parts/section-parts/section-three-col.php
+++ template-parts/section-parts/section-three-col.php
@@ -1,33 +1,40 @@
 <?php
+    // section vars
+    $vertical_padding = get_sub_field('vertical_padding');
+    $texture_top = get_sub_field('texture_top');
+    $texture_bottom = get_sub_field('texture_bottom');
+?>
+
+<?php
 // check if the repeater field has rows of data
 if( have_rows('columns') ):
 ?>
 <!-- 3 Column Section -->
-<div class="container-fluid row-section">
+<div class="container-fluid row-section texture-section">
   <div class="row">
 <?php
  	// loop through the rows of data
   while ( have_rows('columns') ) : the_row();
-  
-    //vars
+
+    //column vars
     $column_background = get_sub_field('column_background');
+    $shade_of_background = get_sub_field('shade_of_background');
     $heading = get_sub_field('heading');
     $url = get_sub_field('url');
+    $overlay = get_sub_field('overlay') . " ";
 ?>
 
-    <div class="col-sm-4 dark-bg" style="background-image: url('<?php echo $column_background; ?>');">
-      <div class="wp-table wp-full-height">
-        <div class="wp-table-cell-middle">
-          <div class="section-info-wrap center">
-            <h4><a href="<?php echo $url; ?>"><?php echo $heading; ?></a></h4>
-          </div>
-        </div>
+    <div class="col-sm-4 no-padding <?php echo $shade_of_background; ?>" style="background-image: url('<?php echo $column_background; ?>');">
+      <div class="text-center <?php echo $overlay . $vertical_padding; ?>">
+        <h4><a href="<?php echo $url; ?>"><?php echo $heading; ?></a></h4>
       </div>
     </div>
 
 <?php
   endwhile;
 ?>
+    <div class="texture-top" style="background-image: url('<?php echo $texture_top; ?>');"></div>
+    <div class="texture-bottom" style="background-image: url('<?php echo $texture_bottom; ?>');"></div>
   </div>
 </div>
 
@@ -36,4 +43,30 @@ if( have_rows('columns') ):
     // no rows found
 endif;
 ?>
+<!-- /3 Column Section -->
+
+
+
+<!-- 3 Column Section -->
+<div class="container-fluid row-section texture-section">
+    <div class="row">
+      <div class="col-sm-4 dark-bg v-center-section-lg" style="background-image: url('images/blue-truck.jpg');">
+        <div class="text-center">
+          <h4 class=""><a href="javascript:void(0)">wheels</a></h4>
+        </div>
+      </div>
+      <div class="col-sm-4 dark-bg v-center-section-lg" style="background-image: url('images/blue-truck.jpg');">
+        <div class="text-center">
+          <h4 class=""><a href="javascript:void(0)">tires</a></h4>
+        </div>
+      </div>
+      <div class="col-sm-4 dark-bg v-center-section-lg" style="background-image: url('images/blue-truck.jpg');">
+        <div class="text-center">
+          <h4 class=""><a href="javascript:void(0)">accessories</a></h4>
+        </div>
+      </div>
+    </div>
+    <div class="texture-top" style="background-image: url('<?php echo $texture_top; ?>');"></div>
+    <div class="texture-bottom" style="background-image: url('<?php echo $texture_bottom; ?>');"></div>
+  </div>
 <!-- /3 Column Section -->
