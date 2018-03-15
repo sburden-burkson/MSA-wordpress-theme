caab7e3e5a32a95774969b115cfa98bb963d30a0
Stephen Burden <spburden@hotmail.com>
2018-03-15 12:24:08 -0700

    update 3 column section

--- template-parts/section-parts/section-three-col.php
+++ template-parts/section-parts/section-three-col.php
@@ -3,6 +3,9 @@
     $vertical_padding = get_sub_field('vertical_padding');
     $texture_top = get_sub_field('texture_top');
     $texture_bottom = get_sub_field('texture_bottom');
+    $column_count = 0;
+    if( have_rows('columns') ): while ( have_rows('columns') ) : $column_count++; the_row();
+    endwhile; endif;
 ?>
 
 <?php
@@ -17,6 +20,13 @@ if( have_rows('columns') ):
   while ( have_rows('columns') ) : the_row();
 
     //column vars
+    if ($column_count == 1) {
+      $column_width = "12";
+    } elseif ($column_count == 2) {
+      $column_width = "6";
+    } else {
+      $column_width = "4";
+    }
     $column_background = get_sub_field('column_background');
     $shade_of_background = get_sub_field('shade_of_background');
     $heading = get_sub_field('heading');
@@ -24,12 +34,13 @@ if( have_rows('columns') ):
     $overlay = get_sub_field('overlay') . " ";
 ?>
 
-    <div class="col-sm-4 no-padding <?php echo $shade_of_background; ?>" style="background-image: url('<?php echo $column_background; ?>');">
-      <div class="text-center <?php echo $overlay . $vertical_padding; ?>">
-        <h4><a href="<?php echo $url; ?>"><?php echo $heading; ?></a></h4>
-      </div>
+    <div class="col-sm-<?php echo $column_width; ?> no-padding <?php echo $shade_of_background; ?>" style="background-image: url('<?php echo $column_background; ?>');">
+      <a href="<?php echo $url; ?>">
+        <div class="text-center <?php echo $overlay . $vertical_padding; ?>">
+          <h4><?php echo $heading; ?></h4>
+        </div>
+      </a>
     </div>
-
 <?php
   endwhile;
 ?>
